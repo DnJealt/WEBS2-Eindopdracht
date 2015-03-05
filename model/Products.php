a5d3cdceefefe1bfda8a04e88263af5aed0258f0
michael <man.schouten@student.avans.nl>
2015-03-05 10:33:50 +0100

    structuur boom, about,footer,head,header,index,product pages start

--- model/Products.php
+++ model/Products.php
@@ -5,49 +5,32 @@ Class Products
     private $price;
     private $size;
 
-    /**
-     * @return mixed
-     */
+    /** * Getters/Setters * **/
     public function getName()
     {
         return $this->name;
     }
 
-    /**
-     * @param mixed $name
-     */
     public function setName($name)
     {
         $this->name = $name;
     }
 
-    /**
-     * @return mixed
-     */
     public function getPrice()
     {
         return $this->price;
     }
 
-    /**
-     * @param mixed $price
-     */
     public function setPrice($price)
     {
         $this->price = $price;
     }
 
-    /**
-     * @return mixed
-     */
     public function getSize()
     {
         return $this->size;
     }
 
-    /**
-     * @param mixed $size
-     */
     public function setSize($size)
     {
         $this->size = $size;
---
+++ view/product.php
@@ -0,0 +1,11 @@
+<?php include_once 'head.php'?>
+
+<body>
+<?php include_once 'header.php'?>
+
+
+
+
+
+<?php include_once 'footer.php' ?>
+</body>
