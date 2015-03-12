afe676b150f5886608ee1edaf6b6d2acfcf4145d
DnJealt <jlm.vantartwijk@student.avans.nl>
2015-03-12 13:11:38 +0100

    Header

--- view/index.php
+++ view/index.php
@@ -3,6 +3,13 @@
 <body>
 <?php include_once 'header.inc.php' ?>
 
+<br>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">Welkom bij dé webshop voor Nutella hazelnootpasta en Calvé pindakaas!</div>
+    </div>
+</div>
 
 
 
