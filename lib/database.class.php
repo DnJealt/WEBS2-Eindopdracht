0271f3296227df7c49ad1ee82088477a68c0ffd4
michael <man.schouten@student.avans.nl>
2015-03-12 13:12:54 +0100

    l

--- lib/database.class.php
+++ lib/database.class.php
@@ -1,31 +1,52 @@
 <?php
 
+//include database constants
+include_once 'config.inc.php';
+
 class Database{
 
     private $mysqli;
     private $result;
 
-    //include database constants
-    //include_once 'config.inc.php'
-
-    public function Database($database)
+    public function open_database()
     {
-       $this->mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        //set glpbal variable scope
+        global $connection;
 
-        if(mysqli_connect_errno()){
-            printf("Connection failed %s\n", mysqli_connect_errno());
-            exit();
+        //Open database connection
+
+        $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME)
+            or die(mysqli_connect_errno());
+    }
+
+    public function execute_statement($statement)
+    {
+        //Get connection from global scope
+        global $connection;
+
+        //Open database connection
+        open_database();
+
+        //Execute database query
+        $result = mysqli_query($connection, $statement)
+            or die(mysqli_connect_errno());
+
+        //Close database connection
+        close_database();
+
+        //Return results
+        return $result;
+    }
+
+    public function close_database()
+    {
+        //Set global variable scope
+        global $connection;
+
+        //Close database connection
+        if(isset($connection))
+        {
+            mysqli_close($connection);
         }
-    }
-
-    public function doSQL($sql){
-         $this->result = $this->mysqli->query($input_query)
-             or die($this->mysqli->error.__LINE__);
-    }
-
-    public function readRecord()
-    {
-        return $this->result->fetch_assoc();
-
     }
 }
