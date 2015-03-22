0f8ae1b210986bdf98e87d285e622a887a856e06
DnJealt <jlm.vantartwijk@student.avans.nl>
2015-03-22 17:29:25 +0100

    footer aangepast + productsingle netter gemaakt

--- resources/views/productSingle.blade.php
+++ resources/views/productSingle.blade.php
@@ -4,11 +4,23 @@
 
 <div class="container">
 
-    <img src="/public/img/productimg/{{$products->prdPicBig}}">
-    <p> {{$products->prdName}} </p>
-    <p> {{$products->prdPrice}} </p>
-    <p> {{$products->prdSummary}} </p>
-    <p> {{$products->prdDescription}} </p>
+
+    <div class="row">
+
+        <div class="col-md-1">
+            <img src="{{ URL::to("/img/productimg/$products->prdPicBig") }}" width="480px" height="320px">
+        </div>
+        <div class="col-md-offset-8">
+            <h2>{{$products->prdName}}</h2>
+            <p> Prijs: €{{$products->prdPrice}} </p>
+            <p> <b>Korte omschrijving:</b> <br>{{$products->prdSummary}} </p>
+            <p> <b>Uitgebreide omschrijving:</b> <br>{{$products->prdDescription}} </p>
+        </div>
+
+
+    </div>
+
+
 
 </div>
 @include('footer')
