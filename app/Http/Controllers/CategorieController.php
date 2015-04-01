f2df45506927a243dd4b7f25f0517bf5e1bb6d32
michael <man.schouten@student.avans.nl>
2015-04-02 01:47:38 +0200

    categorie header not working yet

--- app/Http/Controllers/CategorieController.php
+++ app/Http/Controllers/CategorieController.php
@@ -1,106 +1,109 @@
 <?php namespace App\Http\Controllers;
 
+use App\Categorie;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
 use DB;
 
-class CategorieController extends Controller {
+class CategorieController extends Controller
+{
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-        $parent = 0;
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public static function index()
+    {
+        $mainCat = DB::Select("CALL CatogMenu(0)");
 
-        $levels= DB::raw("CALL CatogMenu('$parent')");
-
-        foreach($levels as $niveau)
-        {
-            $print = $niveau['ctgName'];
-
-            echo($print);
-
+        $menu = array();
+        foreach ($mainCat as $mc) {
+            array_push($menu, $mc);
+            $subCat = DB::Select("CALL CatogMenu($mc->ctgId)");
+            foreach ($subCat as $sc) {
+                array_push($menu, $sc);
+                $subsubCat = DB::Select("CALL CatogMenu($sc->ctgId)");
+                foreach ($subsubCat as $ssc) {
+                    array_push($menu, $ssc);
+                }
+            }
         }
+        //echo for the menu's
+        //foreach ($menu as $m) {
+        //    //var_dump($m);
+        //    echo $m->ctgId . ':   :';
+        //    echo $m->ctgName . ':   :';
+        //    echo $m->ctgSubOf . ':   :';
+        //    echo '<br>';
+        //}
+        return $menu;
+    }
 
-       //if($level['Count'] > 0)
-       //{
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
 
-       //}
-       //elseif($level['Count'] == 0)
-       //{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store()
+    {
+        //
+    }
 
-       //}
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
 
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
 
-	}
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function update($id)
+    {
+        //
+    }
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 
 }
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,18 +1,20 @@
-
 <!--[if lt IE 8]>
-<p class="browserupgrade">U gebruikt een <strong>verouderde</strong> browser. Update <a href="http://browsehappy.com/">uw browser</a>, zodat wij een betere ervaring kunnen leveren.</p>
+<p class="browserupgrade">U gebruikt een <strong>verouderde</strong> browser. Update <a href="http://browsehappy.com/">uw
+    browser</a>, zodat wij een betere ervaring kunnen leveren.</p>
 <![endif]-->
 <nav class="navbar navbar-default navbar-fixed-top">
     <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="{{URL::to('/')}}"><img src="/WEBS2-Eindopdracht/public/img/logo/CalveTella.png" height="100px" width="175px"></a>
+            <a class="navbar-brand" href="{{URL::to('/')}}"><img
+                        src="/WEBS2-Eindopdracht/public/img/logo/CalveTella.png" height="100px" width="175px"></a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
@@ -23,10 +25,33 @@
                 <li><a href="{{URL::to('product')}}">Shop</a></li>
 
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">TBD <span class="caret"></span></a>
+                    <?php use App\Http\Controllers\CategorieController;  $menu = CategorieController::index(); ?>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">TBD
+                        <span class="caret"></span></a>
                     <ul class="dropdown-menu" role="menu">
-                        <li><a href="{{URL::to('#')}}">123#</a></li>
-                        <li><a href="{{URL::to('#')}}">231</a></li>
+                        <?php $count = 1 ?>
+                        <?php $subcount = 1 ?>
+                            <?php count($menu) ?>
+                            <?php  $head = 0 ?>
+                        @foreach($menu as $m)
+                            @if(($m->ctgSubOf == 0))
+                                <li><a href="{{URL::to("categorie/$m->ctgId")}}">{{$m->ctgName}}</a></li>
+                                <?php $count++; $head = $m->ctgId ?>
+                            @elseif($m->ctgSubOf == $head)
+                                <ul>
+                                    <li><a href="{{URL::to("categorie/$m->ctgId")}}">{{$m->ctgName}}</a></li>
+                                    @if($m->ctgSubOf == $menu[$m+1]->ctgSubOf)
+                                    </ul>
+                                    @endif
+                            @endif
+
+                            {{--@if($m->childof == $m->parentId)--}}
+                                {{--<ul>--}}
+
+                                {{--</ul>--}}
+                            {{--@endif--}}
+
+                        @endforeach
                     </ul>
                 </li>
 
@@ -35,18 +60,22 @@
 
             <ul class="nav navbar-nav navbar-right">
                 @if(!Auth::user()) {{--working--}}
-                    <li><a class="login" href="{{URL::to('login')}}" role="button">Login</a> </li>
+                <li><a class="login" href="{{URL::to('login')}}" role="button">Login</a></li>
                 @else
                     <li class="logged_in_Name">Logged in as: <b>{{Auth::User()->username}}</b></li>
 
                     @if(Auth::User()->role_roleId == 1)
                         <li class="gotocms"><a href="{{URL::to('CMS')}}">CMS</a></li>
                     @endif
-                    <li><a class="logout" href="{{URL::to('logout')}}" role="button">Log out</a> </li>
+                    <li><a class="logout" href="{{URL::to('logout')}}" role="button">Log out</a></li>
 
-                    <li><a class="cart" href="{{URL::to('cart')}}" role="button"><img src="/WEBS2-Eindopdracht/public/img/logo/cartIcon.png" height="20px" weight="20px"></a> </li>
+                    <li><a class="cart" href="{{URL::to('cart')}}" role="button"><img
+                                    src="/WEBS2-Eindopdracht/public/img/logo/cartIcon.png" height="20px" weight="20px"></a>
+                    </li>
                 @endif
             </ul>
-        </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
 </nav>
